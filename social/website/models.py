aa78547e345d9755c4050f2aa64ca24c84eee480
catasheep0 <catasheep@gmail.com>
2018-11-30 19:12:33 +0100

    basic model

--- social/website/models.py
+++ social/website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class SocialUser(User):
+    description = models.CharField(max_length=140)
+
+class Post(models.Model):
+    author = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=340)
+    date = models.TimeField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=140)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
