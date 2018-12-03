b7915601259439fcc294a50f6667131de83cfb8c
catasheep0 <catasheep@gmail.com>
2018-12-03 18:59:44 +0100

    .

--- social/website/urls.py
+++ social/website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('profile/<str:login>', views.profile)
 ]
--- social/website/views.py
+++ social/website/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import SocialUser, Post
 # Create your views here.
 
 def index(request):
 
     context = {
-        "posts": Post.objects.select_related("author").order_by("date")
+        "posts": Post.objects.select_related("author").order_by("-date")
     }
     print(context)
-    return render(request, "website/index.html", context)
+    return render(request, "website/timeline.html", context)
+
+def profile(request, login):
+    context = {
+        "profile": get_object_or_404(SocialUser, username=login),
+        "posts": Post.objects.filter(author__username=login).order_by("-date")
+    }
+    return render(request, "website/profile.html", context)
