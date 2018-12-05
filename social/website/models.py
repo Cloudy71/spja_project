c116e7fc0b768be90d6d9393650649d0954470eb
Cloudy <crosska111@gmail.com>
2018-12-05 22:40:39 +0100

    Models temporary change

--- social/website/models.py
+++ social/website/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class SocialUser(User):
     description = models.CharField(max_length=140)
 
+
+class Reaction(models.Model):
+    author = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField(default=0)
+    date = models.DateTimeField(auto_now=True)
+
+
 class Post(models.Model):
     author = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
     content = models.CharField(max_length=340)
     date = models.DateTimeField(auto_now=True)
+    reactions = models.ManyToManyField(Reaction, on_delete=models.CASCADE)
+
 
 class Comment(models.Model):
     author = models.ForeignKey(SocialUser, on_delete=models.CASCADE)
     content = models.CharField(max_length=140)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
-    
+    reactions = models.ManyToManyField(Reaction)
